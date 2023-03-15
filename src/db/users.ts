d2460d31dedf736d43adb40958dfa5106eabf4cf
Ricky Wu <rickywu209@gmail.com>
2023-03-15 00:53:47 -0600

    finish user model and schema with helper functions

--- src/db/users.ts
+++ src/db/users.ts
@@ -13,5 +13,16 @@ const UserSchema = new mongoose.Schema({
 export const UserModel = mongoose.model('User', UserSchema);
 //find users 
 export const getUsers = () => UserModel.find();
+//find one user by email
+export const getUserByEmail = (email: string) => UserModel.findOne({ email });
+//find out if user is logged in or not
+export const getUserBySessionToken = (sessionToken: string) => UserModel.findOne({
+    'authentication.sessionToken': sessionToken,
+});
+export const getUserById = (id: string) => UserModel.findById(id);
+export const createUser = (values: Record<string, any>) => new UserModel(values)
+    .save().then((user) => user.toObject());
+export const deleteUserById = (id: string) => UserModel.findOneAndDelete({ _id: id });
+export const updateUserById = (id: string, values: Record<string, any>) => UserModel.findByIdAndUpdate(id, values);
 
 
