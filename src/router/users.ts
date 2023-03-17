7c4bf242088da7927fc481d86fb7eaa9eb848494
Ricky Wu <rickywu209@gmail.com>
2023-03-17 01:09:24 -0600

    adding a controller to delete users

--- src/router/users.ts
+++ src/router/users.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 
 import { getAllUsers } from '../controllers/users';
+import { isAuthenticated } from '../middlewares';
 
+//adding additional prevention on username showing as cookies
 export default (router: express.Router) => {
-    router.get('/users', getAllUsers);
+    router.get('/users', isAuthenticated, getAllUsers);
 };
