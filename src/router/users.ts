be99410be7485bc85dd1941e5e010860cb775854
Ricky Wu <rickywu209@gmail.com>
2023-03-17 01:12:32 -0600

    adding delete user get route

--- src/router/users.ts
+++ src/router/users.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 
-import { getAllUsers } from '../controllers/users';
+import { getAllUsers, deleteUser } from '../controllers/users';
 import { isAuthenticated } from '../middlewares';
 
 //adding additional prevention on username showing as cookies
 export default (router: express.Router) => {
     router.get('/users', isAuthenticated, getAllUsers);
+    router.get('./users/:id', deleteUser)
 };
