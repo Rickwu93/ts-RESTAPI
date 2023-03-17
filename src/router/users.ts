b07031d2cba9057cce8af93cb3bd141c45479f67
Ricky Wu <rickywu209@gmail.com>
2023-03-17 01:33:10 -0600

    adding in a updateUser controller

--- src/router/users.ts
+++ src/router/users.ts
@@ -4,6 +4,7 @@ import { getAllUsers, deleteUser } from '../controllers/users';
 import { isAuthenticated, isOwner } from '../middlewares';
 
 //adding additional prevention on username showing as cookies
+//order matters
 export default (router: express.Router) => {
     router.get('/users', isAuthenticated, getAllUsers);
     router.delete('/users/:id', isAuthenticated, isOwner, deleteUser);
