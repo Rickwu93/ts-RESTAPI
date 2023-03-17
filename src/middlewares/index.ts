373fc48364412c9f98430796480ba830c41f9a8a
Ricky Wu <rickywu209@gmail.com>
2023-03-17 01:24:15 -0600

    adding isOwner middleware

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -2,6 +2,27 @@ import express from 'express';
 import { get, merge } from 'lodash';
 
 import { getUserBySessionToken } from '../db/users';
+//to confirm only owner can delete cookies
+const isOwner = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    try {
+        const { id } = req.params;
+        const currentUserId = get(req, 'identity._id') as string; //string cause we merged it below
+
+        if (!currentUserId) {
+            return res.sendStatus(403)
+        }
+
+        if (currentUserId.toString() !== id) {
+            return res.sendStatus(403);
+        }
+
+        next();
+
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
+}
 
 // authenticating middleware, async takes in req/res
 export const isAuthenticated = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
